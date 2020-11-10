c25fcdca806f2427c40877f469f73e6c00f7fb12
mwturner611 <mwturner611@gmail.com>
2020-11-09 22:19:46 -0600

    change type

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_MONGO_CONNECTION || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_CONNECTION || "mongodb://localhost/workout", {
   useNewUrlParser: true,});
 
 
